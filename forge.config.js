55d91f9c6a32e0f6d2e732f7267e92a7e864875c
Superchicken962 <58424347+Superchicken962@users.noreply.github.com>
2025-09-25 00:53:25 +0930

    Update forge config

--- forge.config.js
+++ forge.config.js
@@ -46,8 +46,8 @@ module.exports = {
       name: '@electron-forge/publisher-github',
       config: {
         repository: {
-          owner: 'github-user-name',
-          name: 'github-repo-name'
+          owner: 'superchicken962',
+          name: 'music-player-app'
         },
         prerelease: false,
         draft: true
